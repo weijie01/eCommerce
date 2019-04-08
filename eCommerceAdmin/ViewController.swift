b94dc19c3e255bb0adc7d2d67e106bb7450f5841
Weijie Lin <university.linweijie@gmail.com>
2019-04-07 23:12:56 -0700

    added the target for admin

---
+++ eCommerceAdmin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eCommerceAdmin
+//
+//  Created by Weijie Lin on 4/7/19.
+//  Copyright © 2019 Weijie Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
