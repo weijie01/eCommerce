1dc3f617dcc0442ec4709a54e78f4842895340aa
Weijie Lin <university.linweijie@gmail.com>
2019-04-13 23:08:03 -0700

    added custom rounded views

---
+++ Shared/Utils/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  eCommerce
+//
+//  Created by Weijie Lin on 4/13/19.
+//  Copyright © 2019 Weijie Lin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct AppColors {
+    static let DARK_BLUE = #colorLiteral(red: 0.2174814343, green: 0.2681963146, blue: 0.3689454794, alpha: 1)
+    static let RED = #colorLiteral(red: 0.8994478583, green: 0.3572379351, blue: 0.2827754021, alpha: 1)
+    static let OFF_WHITE = #colorLiteral(red: 0.9537276626, green: 0.9489204288, blue: 0.9703347087, alpha: 1)
+}
---
+++ Shared/View/RoundedViews.swift
@@ -0,0 +1,35 @@
+//
+//  RoundedViews.swift
+//  eCommerce
+//
+//  Created by Weijie Lin on 4/13/19.
+//  Copyright © 2019 Weijie Lin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RoundedButton : UIButton {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.cornerRadius = 5
+    }
+}
+
+class RoundedShadowView : UIView {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.cornerRadius = 5
+        layer.shadowColor = AppColors.DARK_BLUE.cgColor
+        layer.shadowOpacity = 0.4
+        layer.shadowOffset = CGSize.zero
+        layer.shadowRadius = 3
+    }
+}
+
+class RoundedImageView : UIImageView {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.cornerRadius = 5
+    }
+}
