826c0bb9064180b1ecf360721ff1137526f78b7f
Weijie Lin <university.linweijie@gmail.com>
2019-04-19 22:47:13 -0700

    added a xib for forget-password

---
+++ eCommerce/Xibs/ForgotPasswordVC.swift
@@ -0,0 +1,25 @@
+//
+//  ForgotPasswordVC.swift
+//  eCommerce
+//
+//  Created by Weijie Lin on 4/19/19.
+//  Copyright © 2019 Weijie Lin. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ForgotPasswordVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func cancelClicked(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func resetClicked(_ sender: Any) {
+        
+    }
+}
