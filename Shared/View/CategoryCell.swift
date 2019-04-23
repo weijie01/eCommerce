466b89bed97bf6f960d7643ca90142f7274e90fc
Weijie Lin <university.linweijie@gmail.com>
2019-04-22 23:47:09 -0700

    added category cell ui

---
+++ Shared/View/CategoryCell.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryCell.swift
+//  eCommerce
+//
+//  Created by Weijie Lin on 4/22/19.
+//  Copyright © 2019 Weijie Lin. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
