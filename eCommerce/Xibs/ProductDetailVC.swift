f83d71433cc79b8fe863c3a8b6613c2152366bb0
Weijie Lin <university.linweijie@gmail.com>
2019-05-03 21:51:39 -0700

    init ProductDetailVC

---
+++ eCommerce/Xibs/ProductDetailVC.swift
@@ -0,0 +1,17 @@
+//
+//  ProductDetailVC.swift
+//  eCommerce
+//
+//  Created by Weijie Lin on 5/3/19.
+//  Copyright © 2019 Weijie Lin. All rights reserved.
+//
+
+import UIKit
+
+class ProductDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
